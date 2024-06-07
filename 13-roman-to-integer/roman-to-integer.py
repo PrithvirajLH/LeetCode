4397d7a925e8ccd0a46efae05f8ed49763196fa1
PrithvirajLH <96643920+PrithvirajLH@users.noreply.github.com>
2024-06-07 11:29:37 -0500

    Time: 38 ms (21.10%) | Memory: 11.7 MB (50.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def romanToInt(self, s):
         maps = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        value = 0
-        for i in range(len(s)):
-            if i < len(s)-1 and maps[s[i]] < maps[s[i+1]]:
-                value -= maps[s[i]]
-            else:
-                value +=maps[s[i]]
+        y = s.replace('IV','IIII').replace('IX','VIIII').replace('XL', 'XXXX').replace('XC','LXXXX').replace('CD','CCCC').replace('CM','DCCCC')
+        value=0
+
+        for x in y:
+            value += maps[x]
         return value
