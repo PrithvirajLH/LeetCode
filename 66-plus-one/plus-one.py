23b907bf655f2290362975a50b3810b6aa91b355
PrithvirajLH <96643920+PrithvirajLH@users.noreply.github.com>
2024-06-10 21:25:59 -0500

    Time: 32 ms (82.32%) | Memory: 16.4 MB (96.01%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -2,6 +2,7 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         if digits[-1] != 9:
             digits[-1] += 1
+            return digits
         else:
             if all(item == 9 for item in digits):
                 ans = [0] * (len(digits)+1)
@@ -13,7 +14,8 @@ class Solution:
                     digits[i] = 0
                     i -= 1
                 digits[i] +=1
-        return digits
+                return digits
+        
         
 
         
