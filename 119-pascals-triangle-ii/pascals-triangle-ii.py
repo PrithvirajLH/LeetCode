9681a631049f17a57353a7540407466404a1ba42
PrithvirajLH <96643920+PrithvirajLH@users.noreply.github.com>
2024-07-01 11:26:31 -0500

    Time: 41 ms (17.72%) | Memory: 16.5 MB (62.25%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        ans = [[1]]
+        for i in range(rowIndex):
+            prev = ans[-1]
+            curr = []
+            curr.append(1)
+            for j in range(len(prev)-1):
+                curr.append(prev[j]+prev[j+1])
+            curr.append(1)
+            ans.append(curr)
+        return ans[-1]
+        
