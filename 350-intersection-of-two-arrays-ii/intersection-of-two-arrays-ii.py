750968d498773cfa2ed9b1dbdcb79b0e5e78a4ff
PrithvirajLH <96643920+PrithvirajLH@users.noreply.github.com>
2024-07-01 20:49:06 -0500

    Time: 56 ms (23.44%) | Memory: 16.6 MB (95.40%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,14 +1,22 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
-        def helper(arr1, arr2):
-            for i in arr1:
-                if i in arr2:
-                    ans.append(i)
-                    arr2.remove(i)
-        if len(nums1) <= len(nums2):
-            helper(nums1,nums2)
-        else:
-            helper(nums2,nums1)
-        return ans
+        # ans = []
+        # def helper(arr1, arr2):
+        #     for i in arr1:
+        #         if i in arr2:
+        #             ans.append(i)
+        #             arr2.remove(i)
+        # if len(nums1) <= len(nums2):
+        #     helper(nums1,nums2)
+        # else:
+        #     helper(nums2,nums1)
+        # return ans
         
+        hashMap = Counter(nums1)
+        ans = []
+
+        for num in nums2:
+            if hashMap[num] > 0:
+                hashMap[num] -= 1
+                ans.append(num)
+        return ans
