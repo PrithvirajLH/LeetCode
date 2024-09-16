e45289bb95563f141bcdf41711906f3a27e8fae7
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2024-09-15 23:55:31 -0500

    Time: 73 ms (19.71%) | Memory: 17.1 MB (54.98%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,15 +1,15 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        already_been = set([0])
-        to_visit = deque(rooms[0])
-        while to_visit.__len__() > 0:
-            room = to_visit.popleft()
+        visit = set()
+
+        def dfs(room):
+            visit.add(room)
             for key in rooms[room]:
-                if key not in already_been:
-                    to_visit.append(key)
-            already_been.add(room)
-            if already_been.__len__() == rooms.__len__():
-                return True
-        return already_been.__len__() == rooms.__len__()
+                if key not in visit:
+                    dfs(key)
+        
+        dfs(0)
+        return len(visit) == len(rooms)
+
 
         
