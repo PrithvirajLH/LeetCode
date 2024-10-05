a0f6fba8769345954f0c48ae3ceb8dc8f784cea4
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2024-10-05 15:27:37 -0500

    Time: 61 ms (87.65%) | Memory: 16.7 MB (33.57%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -1,16 +1,16 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        for_ans = defaultdict(int)
-
-        for i in range(1, n+1):
-            total = 0
-            num = i
-            while num > 0:
-                total += num%10
+        mapping = {}
+        for num in range(1, n+1):
+            sums = 0
+            while num != 0:
+                sums += num % 10
                 num //= 10
-            for_ans[total] += 1
+            mapping[sums] = mapping.get(sums,0) + 1
         
-        max_group = max(for_ans.values())
-        ans = sum(1 for size in for_ans.values() if size == max_group)
-        return ans
-
+        max_val = max(mapping.values())
+        count = 0
+        for k, v in mapping.items():
+            if v == max_val:
+                count += 1
+        return count
