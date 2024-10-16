c5b49403c0bfd852295a0ed5eac922697d96d013
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2024-10-15 21:42:09 -0500

    Time: 447 ms (54.28%) | Memory: 17.5 MB (17.26%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -10,63 +10,33 @@ class Solution:
         if ('a' in wordsDict):
             if (wordsDict['a'] == 5000):
                 return list(range(0, len(s) - 4999))
-        n = len(s)
-        wordsLen = len(words)
-        eachWordLen = len(words[0])
-        wordsTotalLen = wordsLen * eachWordLen
-        for k in range(eachWordLen):
-            i = 0
-            while(i*eachWordLen + wordsTotalLen + k <= n):
-                tempDict = copy.copy(wordsDict)
-                j = wordsLen + i
-                while(j > i):
-                    curWord = s[(j-1)*eachWordLen + k:j*eachWordLen + k]
-                    if curWord in tempDict:
-                        tempDict[curWord] -= 1
-                        if (tempDict[curWord] == 0):
-                            del tempDict[curWord]
-                        j -= 1
-                        if (j == i):
-                            solutions.append(i*eachWordLen + k)
-                            i += 1
-                            break
-                    else:
-                        i = j
-                        break
-        return(solutions)
         
         
         
+        res = []
+        n = len(words)
+        word_len = len(words[0])
+        word = n * word_len
         
+        for i in range(0, len(s)):
+            if i+word > len(s):
+                break
+            sent = s[i:i + word]
+            sent_list = []
+            for k in range(0,len(sent), word_len):
+                if k+word_len <= len(sent):
+                    sub = sent[k:k+word_len]
+                    sent_list.append(sub)
+            flag = True
+            for j in words:
+                if j not in sent_list:
+                    flag = False
+                    break
+                else:
+                    sent_list.remove(j)
+            if flag:
+                res.append(i)
         
-        
-        
-        
-        
-        # res = []
-        # n = len(words)
-        # word_len = len(words[0])
-        # word = n * word_len
-
-        # for i in range(0, len(s)):
-        #     if i+word > len(s):
-        #         break
-        #     sent = s[i:i + word]
-        #     sent_list = []
-        #     for k in range(0,len(sent), word_len):
-        #         if k+word_len <= len(sent):
-        #             sub = sent[k:k+word_len]
-        #             sent_list.append(sub)
-        #     flag = True
-        #     for j in words:
-        #         if j not in sent_list:
-        #             flag = False
-        #             break
-        #         else:
-        #             sent_list.remove(j)
-        #     if flag:
-        #         res.append(i)
-        
-        # return res
+        return res
                 
         
