3a3f442335788e9b3ce98f80b7651eb36a043edf
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2025-03-27 10:21:31 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.87%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,29 +5,40 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = head
-        def reverse(head):
-            prev = None
-            while head:
-                temp = head.next
-                head.next = prev
-                prev = head
-                head = temp
-            return prev
-        prev = reverse(head)
-        second = prev
+        # def reverse(head):
+        #     prev = None
+        #     while head:
+        #         temp = head.next
+        #         head.next = prev
+        #         prev = head
+        #         head = temp
+        #     return prev
+        # prev = reverse(head)
+        # second = prev
 
-        if n == 1:
-            prev = second.next
-        else:
-            count = 1
-            while count != n - 1:
-                second = second.next
-                count  += 1
-            second.next = second.next.next
+        # if n == 1:
+        #     prev = second.next
+        # else:
+        #     count = 1
+        #     while count != n - 1:
+        #         second = second.next
+        #         count  += 1
+        #     second.next = second.next.next
 
 
-        return reverse(prev)
+        # return reverse(prev)
+
+        dummy = ListNode(0)
+        dummy.next = head
+        first = second = dummy
+        for _ in range(n+1):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return dummy.next
         
         
             
